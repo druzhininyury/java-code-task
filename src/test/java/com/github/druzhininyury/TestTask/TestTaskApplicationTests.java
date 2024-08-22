80f7deb74eada0c0c666a64bb824839e3a39c43f
Druzhinin Yury <druzhinin.yy@gmail.com>
2024-08-23 01:03:14 +0300

    Initial commit.

---
+++ src/test/java/com/github/druzhininyury/TestTask/TestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.druzhininyury.TestTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
